edf60a2c08ff0a51a166e21c724300f0ab0b9ad4
MrSahilShaikh <60029854+MrSahilShaikh@users.noreply.github.com>
2022-06-29 12:55:38 +0530

    Add files via upload

---
+++ my own Blockchain.py
@@ -0,0 +1,31 @@
+import hashlib
+def hashGenerator(data):
+    result=hashlib.sha256(data.encode())
+    return result.hexdigest()
+
+class Block:
+    def __init__(self,data,hash,prev_hash):
+        self.data=data
+        self.hash=hash
+        self.prev_hash=prev_hash
+
+class Blockchain:
+    def __init__(self):
+        hashLast=hashGenerator('Sahil')
+        hashStart=hashGenerator('Shaikh')
+
+        genesis=Block('Sahil Shaikh is Blockchain Developer',hashStart,hashLast)
+        self.chain=[genesis]
+
+    def add_block(self,data):
+        prev_hash=self.chain[-1].hash
+        hash=hashGenerator(data+prev_hash)
+        block=Block(data,hash,prev_hash)
+        self.chain.append(block)
+bc=Blockchain()
+bc.add_block('Ethereum')
+bc.add_block('Solana')
+bc.add_block('Rust')
+
+for block in bc.chain:
+    print(block.__dict__)
